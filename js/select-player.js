3a280c53cb988caba21987c2e0a592ae2b1c45eb
Sabid Hasan <sabid.soft@gmail.com>
2022-08-22 12:20:21 +0600

    select player and ulilities  script added

---
+++ js/select-player.js
@@ -0,0 +1,23 @@
+document.getElementById('messi').addEventListener('click', function(event){
+    selectPlayer('messi', 'Lionel Messi')
+})
+
+document.getElementById('neymar').addEventListener('click', function(){
+    selectPlayer('neymar', 'Neymar Jr')
+})
+
+document.getElementById('mbappe').addEventListener('click', function(){
+    selectPlayer('mbappe', 'Kylian Mbappe')
+})
+
+document.getElementById('machado').addEventListener('click', function(){
+    selectPlayer('machado', 'Vitor Machado')
+})
+
+document.getElementById('ramos').addEventListener('click', function(){
+    selectPlayer('ramos', 'Sergio Ramos')
+})
+
+document.getElementById('sanches').addEventListener('click', function(){
+    selectPlayer('sanches', 'Renato Sanches')
+})
---
+++ js/utilities.js
@@ -0,0 +1,25 @@
+function selectPlayer(playerId, playerName){
+    // get selected players ordered list element
+    const selectedPlayers = document.getElementById('selected-players')
+    
+    // check number of players > 5
+    if(selectedPlayers.childNodes.length >=5){
+        return alert("You can't select more than 5 players!")
+    }
+
+    // create player list and append list
+    const li = document.createElement('li')
+    li.innerText = playerName
+    selectedPlayers.appendChild(li)
+
+    // change some style
+    const messi = document.getElementById(playerId)
+    messi.style.background = '#A2A9AF'
+    messi.disabled = true
+
+    // change the style based on child node exist or not
+    if(selectedPlayers.childNodes.length !== 0){
+        const selectionMsg = document.getElementById('selection-msg')
+        selectionMsg.style.display = 'none'
+    }
+}
